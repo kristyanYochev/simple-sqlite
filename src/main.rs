2876dd73fbe9fbe3a6acac085cca46fbdc26b180
Kristiyan Yochev <kris.yochev@gmail.com>
2023-09-17 22:31:48 +0300

    Implemented simple REPL
    Only responds to '.exit'

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        show_prompt();
+
+        let buffer = read_input();
+
+        if buffer == ".exit" {
+            break;
+        } else {
+            println!("Unrecognized command '{}'.", buffer);
+        }
+    }
+}
+
+fn show_prompt() {
+    print!("db > ");
+    std::io::stdout().flush().expect("Cannot flush stdout");
+}
+
+fn read_input() -> String {
+    let mut buffer = String::new();
+
+    std::io::stdin()
+        .read_line(&mut buffer)
+        .expect("Error reading input!");
+
+    buffer.trim().into()
 }
