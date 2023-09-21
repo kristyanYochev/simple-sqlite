4415d22b95ba6dd6f41ebc81ebfba56aec7e02b0
Kristiyan Yochev <kris.yochev@gmail.com>
2023-09-21 12:43:45 +0300

    Added row serialization and deserialization

--- src/sql.rs
+++ src/sql.rs
@@ -1,4 +1,5 @@
-use std::{array::TryFromSliceError, num::ParseIntError};
+use core::slice::SlicePattern;
+use std::{array::TryFromSliceError, mem::size_of, num::ParseIntError};
 
 use thiserror::Error;
 
@@ -57,4 +58,56 @@ pub fn prepare_statement(buffer: &String) -> Result<Statement, PrepareError> {
     } else {
         Err(PrepareError::UnrecognizedStatement(buffer.to_owned()))
     }
+}
+
+impl Row {
+    const ID_OFFSET: usize = 0;
+    const ID_SIZE: usize = size_of_ret_type(|x: Self| x.id);
+    const USERNAME_OFFSET: usize = Self::ID_OFFSET + Self::ID_SIZE;
+    const USERNAME_SIZE: usize = size_of_ret_type(|x: Self| x.username);
+    const EMAIL_OFFSET: usize = Self::USERNAME_OFFSET + Self::USERNAME_SIZE;
+    const EMAIL_SIZE: usize = size_of_ret_type(|x: Self| x.email);
+
+    pub fn copy_to_bytes(&self, dst: &mut [u8]) {
+        let id_bytes = self.id.to_ne_bytes();
+        assert_eq!(Self::ID_SIZE, id_bytes.len());
+        dst[Self::ID_OFFSET..Self::ID_OFFSET + Self::ID_SIZE].copy_from_slice(id_bytes.as_slice());
+
+        let username_start = Self::USERNAME_OFFSET;
+        let username_end = username_start + Self::USERNAME_SIZE;
+        assert_eq!(Self::USERNAME_SIZE, self.username.len());
+        dst[username_start..username_end].copy_from_slice(self.username.as_slice());
+
+        let email_start = Self::EMAIL_OFFSET;
+        let email_end = email_start + Self::EMAIL_SIZE;
+        assert_eq!(Self::EMAIL_SIZE, self.email.len());
+        dst[email_start..email_end].copy_from_slice(self.email.as_slice());
+    }
+
+    pub fn from_bytes(&self, src: &[u8]) -> Row {
+        let id_start = Self::ID_OFFSET;
+        let id_end = id_start + Self::ID_SIZE;
+        let id = u32::from_ne_bytes(src[id_start..id_end].try_into().unwrap());
+
+        let username_start = Self::USERNAME_OFFSET;
+        let username_end = username_start + Self::USERNAME_SIZE;
+        let username = src[username_start..username_end].try_into().unwrap();
+
+        let email_start = Self::EMAIL_OFFSET;
+        let email_end = email_start + Self::EMAIL_SIZE;
+        let email = src[email_start..email_end].try_into().unwrap();
+
+        Row {
+            id,
+            username,
+            email,
+        }
+    }
+}
+
+const fn size_of_ret_type<F, T, U>(_f: F) -> usize
+where
+    F: FnOnce(T) -> U,
+{
+    size_of::<U>()
 }
